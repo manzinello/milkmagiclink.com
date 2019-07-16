dba3062af8fb07caeea728a910e81a1cf6d97536
Matteo Manzinello <matteo@manzinello.com>
2019-07-16 23:39:47 +0200

    milk-title class

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>
+      <h1 className="milk-title">
         <img
           className="zi-avatar milk-logo"
           src="/images/milk.png"
@@ -15,18 +15,21 @@ function App() {
         />
         milk
       </h1>
-      <p class="zi-title">a magic link for your app</p>
-      <div class="zi-card">
+      <p className="zi-title">a magic link for your app</p>
+      <div className="zi-card">
         <p>
           no more double badges, just a single link that opens the correct store
           for the device, try{" "}
           <a href="https://milkmagic.link/twitter">twitter app</a>
         </p>
-        <a href="https://airtable.com/shrIvWsukFBXl0HyM" class="zi-btn success">
+        <a
+          href="https://airtable.com/shrIvWsukFBXl0HyM"
+          className="zi-btn success"
+        >
           Request your magic link
         </a>
       </div>
-      <p class="zi-comment">
+      <p className="zi-comment">
         a project by{" "}
         <a href="https://matteomanzinello.com">matteo manzinello</a>
       </p>
