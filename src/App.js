60d4bb454b7ce4973516d357b2140303a98df6f3
Matteo Manzinello <matteo@manzinello.com>
2019-07-16 23:56:33 +0200

    a lot of changes in texts

--- src/App.js
+++ src/App.js
@@ -18,10 +18,25 @@ function App() {
       <p className="zi-title">a magic link for your app</p>
       <div className="zi-card">
         <p>
-          no more double badges, just a single link that opens the correct store
-          for the device, try{" "}
-          <a href="https://milkmagic.link/twitter">twitter app</a>
+          <h3>What is milk?</h3>
+          Milk gives you a single <strong>magic</strong> link that will redirect
+          the user to the correct store to download your app. Why we have to add
+          two badges for the App Store and Play Store? With a single milk magic
+          link the Android users will see the Play Store and the iOS users the
+          App Store.
+          <pre>https://milkmagic.link/twitter</pre>
+          Obviously instead of <code>twitter</code> you can decide everything
+          you want.
         </p>
+        <p>
+          All links are <strong>human-checked</strong>.
+        </p>
+        <p>
+          <h3>Real examples</h3>
+          <a href="https://milkmagic.link/twitter">twitter </a> or{" "}
+          <a href="https://milkmagic.link/instagram">instagram</a>
+        </p>
+        <br />
         <a
           href="https://airtable.com/shrIvWsukFBXl0HyM"
           className="zi-btn success"
