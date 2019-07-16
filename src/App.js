aa0f1996e7d4597603fd5429427c7482c57dace0
Matteo Manzinello <matteo@manzinello.com>
2019-07-16 20:18:54 +0200

    images and a draft!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from "react";
 
 import "@zeit-ui/style";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>milk</h1>
-      <h3>a magic link for your app</h3>
+      <h1>
+        <img
+          className="zi-avatar milk-logo"
+          src="/images/milk.png"
+          alt="milk"
+        />
+        milk
+      </h1>
+      <p class="zi-title">a magic link for your app</p>
       <div class="zi-card">
-        <h4>a light card</h4>
+        <p>
+          no more double badges, just a single link that opens the correct store
+          for the device, try{" "}
+          <a href="https://milkmagic.link/twitter">twitter app</a>
+        </p>
+        <a href="https://airtable.com/shrIvWsukFBXl0HyM" class="zi-btn success">
+          Request your magic link
+        </a>
       </div>
+      <p class="zi-comment">
+        a project by{" "}
+        <a href="https://matteomanzinello.com">matteo manzinello</a>
+      </p>
     </div>
   );
 }
